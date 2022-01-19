e96f13fb70e1fc7817e5b8daea10595eeab23cb0
yare-yare <Etu_KAMEL.Nouhaila@emsi-edu.ma>
2022-01-19 14:49:40 +0100

    Create Repetitions.cpp

---
+++ Repetitions.cpp
@@ -0,0 +1,26 @@
+//You are given a DNA sequence: a string consisting of characters A, C, G, and T. 
+//Your task is to find the longest repetition in the sequence. 
+//This is a maximum-length substring containing only one type of character.
+//Input
+//The only input line contains a string of n characters.
+//Output
+//Print one integer: the length of the longest repetition.
+
+#include <iostream>
+#include <vector>
+#include<algorithm>
+using namespace std;
+int Repetitions(string DNA){
+  int n;
+         
+  return n;
+}
+
+int main()
+{
+    string DNA;
+    cout << "enter a string : " <<endl;
+    getline(cin,DNA);
+    
+    cout <<Repetitions(DNA)<<endl;
+}
